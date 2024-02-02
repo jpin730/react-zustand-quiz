29fdc14ca02f47bcffa5b8616345f1ba564778ae
Jaime Pineda <jpin730@pm.me>
2024-02-02 13:45:17 -0600

    chore: :wrench: add material ui, roboto font and icons material

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { CssBaseline } from '@mui/material'
+
 import App from './App.tsx'
+
 import './index.css'
+
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <CssBaseline />
     <App />
   </React.StrictMode>,
 )
